4a88650906300bcc68a28e8c045d2f9b9ebe0341
DenisMakovskyi <makovsky.denis@yandex.ru>
2016-11-24 16:22:17 +0200

    Added methods createFifo(), writeNextCommand() in ResultsInterface

--- src/core/interface/results/ResultsInterface.cpp
+++ src/core/interface/results/ResultsInterface.cpp
@@ -1,9 +1,42 @@
 #include "ResultsInterface.h"
 
+ResultsInterface::ResultsInterface(as::io_service &ioService) :
+        mIOService(ioService) {
+    if (!FIFOExists()) {
+        createFifo();
+    }
+    mFIFODescriptor = open(FIFOPath().c_str(), O_WRONLY);
+    if (mFIFODescriptor == -1) {
+        throw IOError("Can not open FIFO file");
+    }
+
+    try {
+        mFIFOStreamDescriptor = new as::posix::stream_descriptor(mIOService, mFIFODescriptor);
+    } catch (const std::exception &e) {
+        throw IOError("Can't assign FIFO descriptor to writer");
+    }
+}
+
+ResultsInterface::~ResultsInterface() {
+    close(mFIFODescriptor);
+    delete mFIFOStreamDescriptor;
+}
+
 void ResultsInterface::createFifo() {
-    if (! FIFOExists()) {
+    if (!FIFOExists()) {
         fs::create_directories(dir());
         mkfifo(FIFOPath().c_str(), 0420);
     }
 }
 
+void ResultsInterface::writeNextCommand() {
+    mFIFOStreamDescriptor->async_write_some(
+            as::buffer(mResultBuffer),
+            boost::bind(&ResultsInterface::writerHandler, this, as::placeholders::error,
+                        as::placeholders::bytes_transferred)
+    );
+}
+
+void ResultsInterface::writerHandler(const boost::system::error_code &error, const size_t bytesTransferred) {
+
+}
--- src/core/interface/results/ResultsInterface.h
+++ src/core/interface/results/ResultsInterface.h
@@ -1,14 +1,35 @@
 #ifndef GEO_NETWORK_CLIENT_RESULTSINTERFACE_H
 #define GEO_NETWORK_CLIENT_RESULTSINTERFACE_H
 
-#include "../BaseFIFOInterface.h"
+#include <vector>
+#include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/bind.hpp>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include "../BaseFIFOInterface.h"
+#include "../../common/exceptions/IOError.h"
 
+namespace as = boost::asio;
 namespace fs = boost::filesystem;
 
-class ResultsInterface: public BaseFIFOInterface {
+class ResultsInterface : public BaseFIFOInterface {
+private:
+    as::io_service &mIOService;
+    as::posix::stream_descriptor *mFIFOStreamDescriptor;
+    vector<char> mResultBuffer;
+
 private:
     void createFifo();
+
+    void writeNextCommand();
+
+    void writerHandler(const boost::system::error_code &error, const size_t bytesTransferred);
+
+public:
+    explicit ResultsInterface(as::io_service &ioService);
+
+    ~ResultsInterface();
 };
 
 #endif //GEO_NETWORK_CLIENT_RESULTSINTERFACE_H
