029f7fa7d8a84caf4231ff23a2381b0c9873abe0
DenisMakovskyi <makovsky.denis@yandex.ru>
2016-12-02 15:40:24 +0200

    Realized http post and get request for registration node in UUID2IP service and further fetching node's address from this service.

--- src/core/network/UUID2IP.cpp
+++ src/core/network/UUID2IP.cpp
@@ -1,52 +1,98 @@
 #include "UUID2IP.h"
 
-UUID2IP::UUID2IP() {
-    boost::uuids::uuid contractor = boost::uuids::random_generator()();
-    mCache.insert(pair < uuids::uuid, pair < string, uint16_t >> (contractor, make_pair(string("127.0.0.1"), 8086)));
+UUID2IP::UUID2IP(string serviceIP, string servicePort) :
+        mServiceIP(serviceIP), mServicePort(servicePort), mResolver(mIOService),
+        mQuery(mServiceIP, mServicePort), mSocket(mIOService), mRequestStream(&mRequest) {
+    mEndpointIterator = mResolver.resolve(mQuery);
+    boost::asio::connect(mSocket, mEndpointIterator);
 }
 
 UUID2IP::~UUID2IP() {
     mCache.clear();
+    socket.close();
 }
 
-void UUID2IP::registerInGlobalCache() {
-    boost::asio::io_service io_service;
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query query("127.0.0.1", "8088");
+void UUID2IP::registerInGlobalCache(uuids::uuid nodeUUID, string nodeIP, string nodePort) {
+    string requestBody = "{\"ip_address\"" + ":" + "\"" + nodeIP + "\"," +
+                         "\"port\"" + ":" + "\"" + nodePort + "\"}";
 
-    tcp::socket socket(io_service);
-    boost::system::error_code error = boost::asio::error::host_not_found;
-    while (error && endpoint_iterator != end) {
-        socket.close();
-        socket.connect(*endpoint_iterator++, error);
+    string uuid = boost::lexical_cast<string>(nodeUUID);
+    string urlParams = "/api/v1/nodes/" + uuid + "/";
+    string host = mServiceIP + ":" + mServicePort;
+    requestStream << "POST " << urlParams << " HTTP/1.0\r\n";
+    requestStream << "Host: " << host << "\r\n";
+    requestStream << "Accept: */*\r\n";
+    requestStream << "Content-Length: " << requestBody.length() << "\r\n";
+    requestStream << "Content-Type: application/json\r\n";
+    requestStream << "Connection: close\r\n\r\n";
+    requestStream << requestBody;
+
+    boost::asio::write(mSocket, mRequest);
+
+    json result = json::parse(processResponse());
+    int code = result.value("code", -1);
+    if (code == 0){
+        json data = json::parse(result.value("data"));
+        string ip = data.value("ip_address");
+        int port = data.value("port");
+        cout << ip << " " << port;
     }
-    if(error){
-        throw boost::system::system_error(error);
-    }
+}
 
-    boost::asio::streambuf request;
-    std::ostream request_stream(&request);
+void UUID2IP::fetchFromGlobalCache(uuids::uuid nodeUUID) {
+    string uuid = boost::lexical_cast<string>(nodeUUID);
+    string urlParams = "/api/v1/nodes/" + uuid + "/";
+    string host = mServiceIP + ":" + mServicePort;
+    requestStream << "GET " << urlParams << " HTTP/1.0\r\n";
+    requestStream << "Host: " << host << "\r\n";
+    requestStream << "Accept: */*\r\n";
+    requestStream << "Connection: close\r\n\r\n";
 
-    string raw = "{\"node_uuid\":\"13e5cf8c-5834-4e52-b65b-f9281dd1ff91\", \"ip_address\":\"198.192.0.1\", \"port\":\"8054\"}";
-    request_stream << "POST / HTTP/1.1\r\n";
-    request_stream << "Host: " << "localhost:3000" << "\r\n";
-    request_stream << "Accept: */*\r\n";
-    request_stream << "Content-Length: " <<  raw.length() << "\r\n";
-    request_stream << "Content-Type: application/x-www-form-urlencoded" << "\r\n";
-    request_stream << "Connection: close\r\n\r\n";
-    request_stream << raw;
+    boost::asio::write(mSocket, mRequest);
 
-    boost::asio::write(socket, request);
+    json result = json::parse(processResponse());
+}
 
+const string UUID2IP::processResponse() {
     boost::asio::streambuf response;
-    boost::asio::read_until(socket, response, "\r\n");
+    std::istream responseStream(&response);
+
+    boost::asio::read_until(mSocket, response, "\r\n");
+
+    std::string httpVersion;
+    responseStream >> httpVersion;
+    unsigned int statusCode;
+    responseStream >> statusCode;
+    std::string statusMessage;
+    responseStream >> statusMessage;
+    if (!responseStream || httpVersion.substr(0, 5) != "HTTP/") {
+        throw ValueError("Invalid response from the remote service");
+    }
+
+    if (statusCode == 200) {
+        boost::asio::read_until(socket, response, "\r\n\r\n");
+
+        string header;
+        while (std::getline(responseStream, header) && header != "\r") {
+            cout << header << endl;
+            cout << endl;
+        }
+
+        if (response.size() > 0) {
+            std::string data((std::istreambuf_iterator<char>(&response)), std::istreambuf_iterator<char>());
+            return data;
+        } else {
+            throw ValueError("Empty response body from the remote service");
+        }
+    } else {
+        throw IOError("Bad request to the remote service");
+    }
 }
 
 const pair <string, uint16_t> &UUID2IP::getNodeAddress(const uuids::uuid &contractorUuid) const {
     if (isNodeAddressExistInLocalCache(contractorUuid)) {
         return mCache.at(contractorUuid);
     }
-    //What value must be returned in case if uuid in cache not founded
     return make_pair("", 0);
 }
 
@@ -55,4 +101,6 @@ const bool UUID2IP::isNodeAddressExistInLocalCache(const uuids::uuid &nodeUuid) 
 }
 
 void UUID2IP::compressLocalCache() {}
+
+
 
--- src/core/network/UUID2IP.h
+++ src/core/network/UUID2IP.h
@@ -9,10 +9,13 @@
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include "Communicator.h"
+#include "../../libs/json.h"
+#include "../common/exceptions/ValueError.h"
 
 using namespace std;
 using boost::asio::ip::tcp;
+using boost::asio::ip::address;
+using json = nlohmann::json;
 
 namespace uuids = boost::uuids;
 
@@ -21,12 +24,26 @@ class UUID2IP {
 private:
     map <uuids::uuid, pair<string, uint16_t >> mCache;
 
+    string mServiceIP;
+    string mServicePort;
+    boost::asio::io_service mIOService;
+    tcp::resolver mResolver;
+    tcp::resolver mQuery;
+    tcp::resolver::iterator mEndpointIterator;
+    tcp::socket mSocket;
+    boost::asio::streambuf mRequest;
+    std::ostream mRequestStream;
+
 private:
     UUID2IP();
 
     ~UUID2IP();
 
     void registerInGlobalCache();
+
+    void fetchFromGlobalCache();
+
+    const string processResponse();
 
     const pair<string, uint16_t> &getNodeAddress(const uuids::uuid &contractorUuid) const;
 
