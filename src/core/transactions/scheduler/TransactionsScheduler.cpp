10f0341d2d1d97a11dc58dcc6228e192b22b432b
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2016-12-30 05:14:32 +0200

    TransactionsScheduler review

--- src/core/transactions/scheduler/TransactionsScheduler.cpp
+++ src/core/transactions/scheduler/TransactionsScheduler.cpp
@@ -1,37 +1,40 @@
 #include "TransactionsScheduler.h"
 
 TransactionsScheduler::TransactionsScheduler(
-        as::io_service &IOService,
-        function_callback managerCallback,
-        Logger *logger) :
+    as::io_service &IOService,
+    function_callback managerCallback,
+    Logger *logger) :
 
-        mIOService(IOService),
-        mMangerCallback(managerCallback),
-        mLog(logger) {
+    mIOService(IOService),
+    mMangerCallback(managerCallback),
+    mLog(logger) {
 
     try {
         mProcessingTimer = new as::deadline_timer(
-                mIOService, boost::posix_time::milliseconds(2 * 1000));
+            mIOService, boost::posix_time::milliseconds(2 * 1000));
 
-    } catch (exception &) {
+    } catch (exception &) { // todo: -> bad_alloc
+        // todo: call delete mProcessingTimer
         throw MemoryError(
-                "TransactionsManager::TransactionsScheduler: "
-                        "Can't allocate enough space for processing timer.");
+            "TransactionsScheduler::TransactionsScheduler");
     }
 
-    mStorage = new storage::UUIDMapBlockStorage("storage", "transactions.bin");
+    // todo: this code can also throw bad_alloc
+    mStorage = new storage::UUIDMapBlockStorage("storage", "transactions.bin"); // todo: the path should be io/transactions/transactions.dat
 }
 
 TransactionsScheduler::~TransactionsScheduler() {
+    //todo: mStorage would not be nullptr by default.
     if (mStorage != nullptr) {
         delete mStorage;
     }
 }
 
 void TransactionsScheduler::addTransaction(
-        BaseTransaction::Shared transaction) {
+    BaseTransaction::Shared transaction) {
 
-    try{
+    try {
+        // todo: is this code needed?
         /*auto transactionContext = transaction.get()->serializeContext();
         mStorage->write(storage::uuids::uuid(transaction.get()->uuid()), transactionContext.first, transactionContext.second);*/
         launchTransaction(transaction);
@@ -45,7 +48,7 @@ void TransactionsScheduler::addTransaction(
 void TransactionsScheduler::run() {
 
     if (!mTransactions.empty()) {
-        try{
+        try {
             launchTransaction(findTransactionWithMinimalTimeout().first);
 
         } catch (std::exception &e) {
@@ -54,26 +57,30 @@ void TransactionsScheduler::run() {
         }
 
     } else {
-        sleepFor(kDefaultDelay);
+        sleepFor(kDefaultDelay); // todo: there should be no default delay!
     }
 }
 
 void TransactionsScheduler::launchTransaction(
-        BaseTransaction::Shared transaction) {
+    BaseTransaction::Shared transaction) {
 
-    pair<CommandResult::SharedConst, TransactionState::SharedConst> transactionResult = transaction.get()->run();
+    // todo: pair<CommandResult::SharedConst, TransactionState::SharedConst> transactionResult -> auto transactionResult
+    pair<CommandResult::SharedConst, TransactionState::SharedConst> transactionResult = transaction.get()->run(); // todo: use ->
     if (!isTransactionInScheduler(transaction)) {
+        // todo: would the transaction be saved to the storage?
         mTransactions.insert(make_pair(transaction, transactionResult.second));
     }
     handleTransactionResult(transaction, transactionResult);
 }
 
+// todo: document all the exceptions may be thrown
 void TransactionsScheduler::handleTransactionResult(
-        BaseTransaction::Shared transaction,
-        pair<CommandResult::SharedConst, TransactionState::SharedConst> result) {
+    BaseTransaction::Shared transaction,
+    pair<CommandResult::SharedConst, TransactionState::SharedConst> result) {
 
+    // todo: please, document the logic of the method.
     if (result.first.get() == nullptr) {
-        if (isTransactionInScheduler(transaction)){
+        if (isTransactionInScheduler(transaction)) {
             auto it = mTransactions.find(transaction);
             it->second = result.second;
             /*auto transactionContext = transaction.get()->serializeContext();
@@ -81,9 +88,10 @@ void TransactionsScheduler::handleTransactionResult(
 
         } else {
             throw ValueError("TransactionsManager::TransactionsScheduler"
-                                     "Transaction reference must be store in memory");
+                                 "Transaction reference must be store in memory");
         }
-        sleepFor(findTransactionWithMinimalTimeout().second);
+
+        sleepFor(findTransactionWithMinimalTimeout().second); // todo: what if no more transactions?
 
     } else if (result.second.get() == nullptr) {
         mMangerCallback(result.first);
@@ -93,34 +101,37 @@ void TransactionsScheduler::handleTransactionResult(
 
         } else {
             throw ValueError("TransactionsManager::TransactionsScheduler"
-                                     "Transaction reference must be store in memory");
+                                 "Transaction reference must be store in memory");
         }
         sleepFor(findTransactionWithMinimalTimeout().second);
 
     } else if (result.first.get() == nullptr && result.second.get() == nullptr) {
         throw ConflictError("TransactionsManager::TransactionsScheduler"
-                                 "Command result and transaction state may not be null pointer references at the same time");
+                                "Command result and transaction state may not be null pointer references at the same time");
 
     } else if (result.first.get() != nullptr && result.second.get() != nullptr) {
         throw ConflictError("TransactionsManager::TransactionsScheduler"
-                                    "Transaction cat not has command result and transaction state at the same time");
+                                "Transaction cat not has command result and transaction state at the same time");
     }
 }
 
 pair<BaseTransaction::Shared, timeout> TransactionsScheduler::findTransactionWithMinimalTimeout() {
 
     BaseTransaction::Shared transaction;
-    timeout minimalTimeout = posix_time::milliseconds(0);
+    timeout minimalTimeout = posix_time::milliseconds(0); // todo: may it be const?
     for (auto &it : mTransactions) {
         auto transactionStateValue = it.second;
         if (transactionStateValue.get() != nullptr) {
             if (transactionStateValue.get()->timeout() > 0) {
+                // todo: there is no comparison with previous timeout
+                // is it calculates the minimum timeout correct?
                 minimalTimeout = posix_time::milliseconds(transactionStateValue.get()->timeout());
                 transaction = it.first;
             }
         }
     }
 
+    // todo: there is no default timeout in the system.
     if (minimalTimeout == posix_time::milliseconds(0)) {
         minimalTimeout = kDefaultDelay;
 
@@ -129,7 +140,8 @@ pair<BaseTransaction::Shared, timeout> TransactionsScheduler::findTransactionWit
     for (auto &it : mTransactions) {
         auto transactionStateValue = it.second;
         if (transactionStateValue.get() != nullptr) {
-            if (transactionStateValue.get()->timeout() > 0 && posix_time::milliseconds(transactionStateValue.get()->timeout()) < minimalTimeout) {
+            if (transactionStateValue.get()->timeout() > 0 &&
+                posix_time::milliseconds(transactionStateValue.get()->timeout()) < minimalTimeout) {
                 minimalTimeout = posix_time::milliseconds(transactionStateValue.get()->timeout());
                 transaction = it.first;
             }
@@ -141,25 +153,26 @@ pair<BaseTransaction::Shared, timeout> TransactionsScheduler::findTransactionWit
 }
 
 void TransactionsScheduler::sleepFor(
-        timeout delay) {
+    timeout delay) {
 
+    // todo: add previous operation cancelling.
     mProcessingTimer->expires_from_now(delay);
     mProcessingTimer->async_wait(
-            boost::bind(
-                    &TransactionsScheduler::handleSleep,
-                    this,
-                    as::placeholders::error,
-                    delay));
+        boost::bind(
+            &TransactionsScheduler::handleSleep, this,
+            as::placeholders::error, delay));
 }
 
 void TransactionsScheduler::handleSleep(
-        const boost::system::error_code &error,
-        timeout delay) {
+    const boost::system::error_code &error,
+    timeout delay) {
 
     if (error) {
+        // todo: no log - no problems?
         return;
 
     } else {
+        // todo: there is no default timeout
         if (delay > kDefaultDelay) {
             run();
         }
@@ -167,7 +180,7 @@ void TransactionsScheduler::handleSleep(
 }
 
 bool TransactionsScheduler::isTransactionInScheduler(
-        BaseTransaction::Shared transaction) {
+    BaseTransaction::Shared transaction) {
 
     return mTransactions.count(transaction) > 0;
 }
--- src/core/transactions/scheduler/TransactionsScheduler.h
+++ src/core/transactions/scheduler/TransactionsScheduler.h
@@ -17,33 +17,43 @@
 
 #include <map>
 
+
 using namespace std;
 
 namespace as = boost::asio;
-
 namespace storage = db::uuid_map_block_storage;
 
-typedef boost::function<void(CommandResult::SharedConst)> function_callback;
-typedef boost::posix_time::time_duration timeout;
-typedef storage::byte byte;
+
+// todo: all this masks must be private for the TransactionsScheduler
+// todo: what is the purpose of this callback? can it be obvious or commented?
+typedef boost::function<void(CommandResult::SharedConst)> function_callback; // todo: CamelCase?
+typedef boost::posix_time::time_duration timeout; // todo: CamelCase?
+typedef storage::byte byte; // see Types.h
 
 namespace posix_time = boost::posix_time;
+
 
 class TransactionsManager;
 
 class TransactionsScheduler {
+    // todo: hsc: tests?
+
+    // todo: please, no friend-based OOP
     friend class TransactionsManager;
 
+    // public members must be at the top, and then - private
 private:
+    // there is no default delay should be present in the system
     const timeout kDefaultDelay = posix_time::milliseconds(200);
 
 private:
+    // todo: separate internal and external objects and pointers
+    // may be there some order?
     as::io_service &mIOService;
     as::deadline_timer *mProcessingTimer;
 
 
-
-    function_callback mMangerCallback;
+    function_callback mMangerCallback; // todo: man(A)gerCallback?
 
     map<BaseTransaction::Shared, TransactionState::SharedConst> mTransactions;
 
@@ -53,35 +63,35 @@ private:
 
 private:
     TransactionsScheduler(
-            as::io_service &IOService,
-            function_callback managerCallback,
-            Logger *logger);
-
+        as::io_service &IOService,
+        function_callback managerCallback,
+        Logger *logger);
     ~TransactionsScheduler();
+
 private:
-    void addTransaction(
-            BaseTransaction::Shared transaction);
+    void addTransaction( // todo: rename to scheduleTransaction()
+        BaseTransaction::Shared transaction);
 
     void run();
 
     void launchTransaction(
-            BaseTransaction::Shared transaction);
+        BaseTransaction::Shared transaction);
 
     void handleTransactionResult(
-            BaseTransaction::Shared transaction,
-            pair<CommandResult::SharedConst, TransactionState::SharedConst> result);
+        BaseTransaction::Shared transaction,
+        pair<CommandResult::SharedConst, TransactionState::SharedConst> result);
 
     pair<BaseTransaction::Shared, timeout> findTransactionWithMinimalTimeout();
 
     void sleepFor(
-            timeout delay);
+        timeout delay);
 
     void handleSleep(
-            const boost::system::error_code &error,
-            timeout delay);
+        const boost::system::error_code &error,
+        timeout delay);
 
     bool isTransactionInScheduler(
-            BaseTransaction::Shared transaction);
+        BaseTransaction::Shared transaction);
 
 };
 
