9a1d349692faa8e8e7b487a191ea62ab28f96b67
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2017-01-18 21:41:38 +0200

    Trust Line refactoring

--- src/core/common/Types.h
+++ src/core/common/Types.h
@@ -1,6 +1,13 @@
 #ifndef GEO_NETWORK_CLIENT_TYPES_H
 #define GEO_NETWORK_CLIENT_TYPES_H
 
+
+/*
+ * Trust lines types
+ */
+#include <boost/multiprecision/cpp_int.hpp>
+
+
 #include <cstdint>
 #include <vector>
 
@@ -15,5 +22,14 @@ enum TrustLineDirection {
     Both     = 3,
 };
 typedef uint8_t SerializedTrustLineDirection;
+
+
+
+/*
+ * Trust lines types
+ */
+typedef boost::multiprecision::checked_uint256_t TrustLineAmount;
+typedef boost::multiprecision::checked_uint256_t TrustLineBlockAmount;
+typedef boost::multiprecision::int256_t TrustLineBalance;
 
 #endif //GEO_NETWORK_CLIENT_TYPES_H
