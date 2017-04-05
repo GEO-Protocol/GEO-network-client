2c990ad48c48f5ff7d66af67ddf39d97e176cda2
denis <vasilov345@gmail.com>
2017-04-05 15:15:00 +0300

    Refactor Four Nodes

--- src/core/transactions/transactions/cycles/FourNodes/CyclesFourNodesResponseTransaction.cpp
+++ src/core/transactions/transactions/cycles/FourNodes/CyclesFourNodesResponseTransaction.cpp
@@ -1,55 +1,46 @@
 #include "CyclesFourNodesResponseTransaction.h"
 
-CyclesFourNodesResponseTransaction::CyclesFourNodesResponseTransaction(TransactionsScheduler *scheduler)
-    : UniqueTransaction(BaseTransaction::TransactionType::Cycles_FourNodesResponseTransaction, scheduler) {
-
-}
-
 CyclesFourNodesResponseTransaction::CyclesFourNodesResponseTransaction(
     const NodeUUID &nodeUUID,
     FourNodesBalancesRequestMessage::Shared message,
-    TransactionsScheduler *scheduler,
     TrustLinesManager *manager,
-    Logger *logger)
-    : UniqueTransaction(BaseTransaction::TransactionType::Cycles_FourNodesResponseTransaction, nodeUUID, scheduler),
-      mTrustLinesManager(manager),
-      mLogger(logger),
-      mRequestMessage(message) {
-
-}
+    Logger *logger) :
+    BaseTransaction(
+        BaseTransaction::TransactionType::Cycles_FourNodesResponseTransaction,
+        nodeUUID),
+    mTrustLinesManager(manager),
+    mLogger(logger),
+    mRequestMessage(message)
+{}
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wconversion"
 TransactionResult::SharedConst CyclesFourNodesResponseTransaction::run() {
 //    Get neighbors UUID from message
-    set<NodeUUID> neighbors = mRequestMessage->Neighbors();
+    const auto kNeighbors = mRequestMessage->Neighbors();
 //    Create message and reserve memory for neighbors
     const auto kMessage = make_shared<FourNodesBalancesResponseMessage>(
         mNodeUUID,
         mTransactionUUID,
-        neighbors.size()
+        kNeighbors.size()
     );
     vector<pair<NodeUUID, TrustLineBalance>> neighborUUIDAndBalance;
-    TrustLineBalance contractorBalance = mTrustLinesManager->balance(mRequestMessage->senderUUID());
+    const auto contractorBalance = mTrustLinesManager->balance(mRequestMessage->senderUUID());
     TrustLineBalance zeroBalance = 0;
     TrustLineBalance stepNodeBalance;
     bool searchDebtors = true;
     if (contractorBalance < zeroBalance)
         searchDebtors = false;
-//    todo Add resize to AddNeighborUUIDAndBalance
-    for (auto &value: neighbors) {
-        stepNodeBalance = mTrustLinesManager->balance(value);
+    for (auto &nodeUUID: kNeighbors) {
+        stepNodeBalance = mTrustLinesManager->balance(nodeUUID);
         if ((searchDebtors and (stepNodeBalance > zeroBalance)) or
             (not searchDebtors and (stepNodeBalance < zeroBalance)))
             kMessage->AddNeighborUUIDAndBalance(
                 make_pair(
-                    value,
+                    nodeUUID,
                     stepNodeBalance));
     }
     sendMessage(mRequestMessage->senderUUID(), kMessage);
     return finishTransaction();
 }
 #pragma clang diagnostic pop
-
-
-
--- src/core/transactions/transactions/cycles/FourNodes/CyclesFourNodesResponseTransaction.h
+++ src/core/transactions/transactions/cycles/FourNodes/CyclesFourNodesResponseTransaction.h
@@ -1,26 +1,23 @@
 #ifndef GEO_NETWORK_CLIENT_CYCLEFOURNODESRESPONSETRANSACTION_H
 #define GEO_NETWORK_CLIENT_CYCLEFOURNODESRESPONSETRANSACTION_H
-#include "../../base/UniqueTransaction.h"
+
+#include "../../base/BaseTransaction.h"
 #include "../../../../trust_lines/manager/TrustLinesManager.h"
 #include "../../../../network/messages/cycles/FourNodes/FourNodesBalancesRequestMessage.h"
 #include "../../../../network/messages/cycles/FourNodes/FourNodesBalancesResponseMessage.h"
+
 #include <set>
 
-class CyclesFourNodesResponseTransaction : public UniqueTransaction {
+class CyclesFourNodesResponseTransaction :
+    public BaseTransaction{
 public:
     CyclesFourNodesResponseTransaction(
         const NodeUUID &nodeUUID,
         FourNodesBalancesRequestMessage::Shared message,
-        TransactionsScheduler *scheduler,
         TrustLinesManager *manager,
         Logger *logger);
 
-    CyclesFourNodesResponseTransaction(TransactionsScheduler *scheduler);
-
     TransactionResult::SharedConst run();
-
-    pair<BytesShared, size_t> serializeToBytes() const {};
-
 
 protected:
 //    Nodes Balances that are mutual between core node and contract node
