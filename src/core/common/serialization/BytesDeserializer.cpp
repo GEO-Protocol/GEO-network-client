1681964c45b9456154761adb56f836906df3fec5
Mykola Ilashchuk <mykola.ilashchuk@gmail.com>
2017-04-18 17:14:21 +0300

    fixed bugs after merge; change signature of serializeToBytes

--- src/core/common/serialization/BytesDeserializer.cpp
+++ src/core/common/serialization/BytesDeserializer.cpp
@@ -51,7 +51,7 @@ void BytesDeserializer::copyInto (
     const size_t bytesCount)
     noexcept
 {
-    cout << "\n\n\n\n\n" << int(mCurrentOffset);
+
     memcpy(
         destination,
         buffer.get() + mCurrentOffset,
