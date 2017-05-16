05a09f3c32579691b24c0ada25c1af2bbfebd697
denis <vasilov345@gmail.com>
2017-05-16 15:28:39 +0300

    Close Trustline command. Change response code to 405.Change command identifier

--- src/core/interface/commands_interface/commands/trust_lines/CloseTrustLineCommand.cpp
+++ src/core/interface/commands_interface/commands/trust_lines/CloseTrustLineCommand.cpp
@@ -22,7 +22,7 @@ CloseTrustLineCommand::CloseTrustLineCommand(
 
 const string &CloseTrustLineCommand::identifier() {
 
-    static const string identifier = "REMOVE:contractors/trust-lines";
+    static const string identifier = "CLOSE:contractors/trust-lines";
     return identifier;
 }
 
@@ -86,7 +86,6 @@ void CloseTrustLineCommand::parse(
             CommandUUID::kHexSize
         );
         mContractorUUID = boost::lexical_cast<uuids::uuid>(hexUUID);
-
     } catch (...) {
         throw ValueError("CloseTrustLineCommand::parse: "
                              "Can't parse command. Error occurred while parsing 'Contractor UUID' token.");
