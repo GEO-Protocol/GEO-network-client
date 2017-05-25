4e53fbbed9bc95626fc60344f1e4a778077c793d
Mykola Ilashchuk <mykola.ilashchuk@gmail.com>
2017-05-25 19:03:02 +0300

    Add filters into history: initial commit

--- src/core/interface/commands_interface/commands/history/HistoryTrustLinesCommand.cpp
+++ src/core/interface/commands_interface/commands/history/HistoryTrustLinesCommand.cpp
@@ -42,9 +42,10 @@ void HistoryTrustLinesCommand::parse(
         throw ValueError("HistoryTrustLinesCommand::parse: "
                                  "Can't parse command. Error occurred while parsing  'from' token.");
     }
+    size_t nextTabSeparator = command.find("\t", tabSeparator + 1);
     string historyCountStr = command.substr(
         tabSeparator + 1,
-        command.size() - 1);
+        nextTabSeparator);
     if (historyCountStr.at(0) == '-') {
         throw ValueError("HistoryTrustLinesCommand::parse: "
                                  "Can't parse command. 'count' token can't be negative.");
@@ -54,6 +55,41 @@ void HistoryTrustLinesCommand::parse(
     } catch (...) {
         throw ValueError("HistoryTrustLinesCommand::parse: "
                                  "Can't parse command. Error occurred while parsing 'count' token.");
+    }
+    tabSeparator = nextTabSeparator;
+    nextTabSeparator = command.find("\t", tabSeparator + 1);
+    string timeFromStr = command.substr(
+        tabSeparator + 1,
+        nextTabSeparator);
+    if (timeFromStr == kNullParameter) {
+        mIsTimeFromPresent = false;
+    } else {
+        mIsTimeFromPresent = true;
+        try {
+            int64_t timeFrom = std::stoul(timeFromStr);
+            mTimeFrom = pt::time_from_string("1970-01-01 00:00:00.000");
+            mTimeFrom += pt::microseconds(timeFrom);
+        } catch (...) {
+            throw ValueError("HistoryTrustLinesCommand::parse: "
+                                 "Can't parse command. Error occurred while parsing 'timeFrom' token.");
+        }
+    }
+    tabSeparator = nextTabSeparator;
+    string timeToStr = command.substr(
+        tabSeparator + 1,
+        command.size() - 1);
+    if (timeToStr == kNullParameter) {
+        mIsTimeToPresent = false;
+    } else {
+        mIsTimeToPresent = true;
+        try {
+            int64_t timeTo = std::stoul(timeToStr);
+            mTimeTo = pt::time_from_string("1970-01-01 00:00:00.000");
+            mTimeTo += pt::microseconds(timeTo);
+        } catch (...) {
+            throw ValueError("HistoryTrustLinesCommand::parse: "
+                                 "Can't parse command. Error occurred while parsing 'timeTo' token.");
+        }
     }
 }
 
@@ -65,6 +101,26 @@ const size_t HistoryTrustLinesCommand::historyFrom() const
 const size_t HistoryTrustLinesCommand::historyCount() const
 {
     return mHistoryCount;
+}
+
+const DateTime HistoryTrustLinesCommand::timeFrom() const
+{
+    return mTimeFrom;
+}
+
+const DateTime HistoryTrustLinesCommand::timeTo() const
+{
+    return mTimeTo;
+}
+
+const bool HistoryTrustLinesCommand::isTimeFromPresent() const
+{
+    return mIsTimeFromPresent;
+}
+
+const bool HistoryTrustLinesCommand::isTimeToPresent() const
+{
+    return mIsTimeToPresent;
 }
 
 CommandResult::SharedConst HistoryTrustLinesCommand::resultOk(string &historyTrustLinesStr) const
