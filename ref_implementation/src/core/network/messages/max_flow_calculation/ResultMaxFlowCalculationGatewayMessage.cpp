570434a1a10b5c4e51921fa8fbe4c6cb999536bd
Danylo Kondratiuk <41803041+BasslineJunkie@users.noreply.github.com>
2018-08-28 17:10:24 +0300

    Update ResultMaxFlowCalculationGatewayMessage.cpp

--- ref_implementation/src/core/network/messages/max_flow_calculation/ResultMaxFlowCalculationGatewayMessage.cpp
+++ ref_implementation/src/core/network/messages/max_flow_calculation/ResultMaxFlowCalculationGatewayMessage.cpp
@@ -1,9 +1,9 @@
 /**
- * This file is part of GEO Project.
+ * This file is part of GEO Protocol.
  * It is subject to the license terms in the LICENSE.md file found in the top-level directory
- * of this distribution and at https://github.com/GEO-Project/GEO-Project/blob/master/LICENSE.md
+ * of this distribution and at https://github.com/GEO-Protocol/GEO-network-client/blob/master/LICENSE.md
  *
- * No part of GEO Project, including this file, may be copied, modified, propagated, or distributed
+ * No part of GEO Protocol, including this file, may be copied, modified, propagated, or distributed
  * except according to the terms contained in the LICENSE.md file.
  */
 
