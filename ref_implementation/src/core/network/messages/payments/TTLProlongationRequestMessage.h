61ac1251f6232a8918b1ab327c681b55ebf96ca5
Danylo Kondratiuk <41803041+BasslineJunkie@users.noreply.github.com>
2018-08-28 17:01:11 +0300

    Update TTLProlongationRequestMessage.h

--- ref_implementation/src/core/network/messages/payments/TTLProlongationRequestMessage.h
+++ ref_implementation/src/core/network/messages/payments/TTLProlongationRequestMessage.h
@@ -1,12 +1,11 @@
 /**
- * This file is part of GEO Project.
+ * This file is part of GEO Protocol.
  * It is subject to the license terms in the LICENSE.md file found in the top-level directory
- * of this distribution and at https://github.com/GEO-Project/GEO-Project/blob/master/LICENSE.md
+ * of this distribution and at https://github.com/GEO-Protocol/GEO-network-client/blob/master/LICENSE.md
  *
- * No part of GEO Project, including this file, may be copied, modified, propagated, or distributed
+ * No part of GEO Protocol, including this file, may be copied, modified, propagated, or distributed
  * except according to the terms contained in the LICENSE.md file.
  */
-
 #ifndef GEO_NETWORK_CLIENT_TTLPROLONGATIONREQUESTMESSAGE_H
 #define GEO_NETWORK_CLIENT_TTLPROLONGATIONREQUESTMESSAGE_H
 
