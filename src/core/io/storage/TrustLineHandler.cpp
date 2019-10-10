88ca1532cd6b255702019281d493ea86a9c39f39
mykola <mykola.ilashchuk@gmail.com>
2019-10-10 15:33:04 +0300

    Fix of TL removing;

--- src/core/io/storage/TrustLineHandler.cpp
+++ src/core/io/storage/TrustLineHandler.cpp
@@ -139,7 +139,7 @@ void TrustLineHandler::deleteTrustLine(
     ContractorID contractorID,
     const SerializedEquivalent equivalent)
 {
-    string query = "DELETE FROM " + mTableName + " WHERE id = ? AND equivalent = ?";
+    string query = "DELETE FROM " + mTableName + " WHERE contractor_id = ? AND equivalent = ?";
     sqlite3_stmt *stmt;
     int rc = sqlite3_prepare_v2( mDataBase, query.c_str(), -1, &stmt, nullptr);
     if (rc != SQLITE_OK) {
